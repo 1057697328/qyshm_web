bd657ac56f797164812a78d80b4bb8807c18b359
clarence <1057697328@qq.com>
2019-05-13 15:20:30 +0800

    Initial commit

---
+++ src/main/java/net/lightwing/qyshm_web/QyshmWebApplication.java
@@ -0,0 +1,13 @@
+package net.lightwing.qyshm_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QyshmWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QyshmWebApplication.class, args);
+    }
+
+}
